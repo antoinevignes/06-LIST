824d94c04ae27b11ba6dc54081bfd72588b76535
Antoine Vignes <168306302+antoinevignes@users.noreply.github.com>
2025-01-23 10:13:24 +0100

    Ajout fonctionnalités: couleur et taille de la police

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -14,12 +14,74 @@ const List = () => {
           dispatch({ type: "updateInput", payload: e.target.value })
         }
       />
+
+      <fieldset>
+        <legend>Choisir une couleur de texte :</legend>
+        <div>
+          <label htmlFor="palevioletred">Pale Violet Red</label>
+          <input
+            type="radio"
+            name="color"
+            id="palevioletred"
+            checked={state.color === "palevioletred"}
+            onChange={() =>
+              dispatch({ type: "changeColor", payload: "palevioletred" })
+            }
+          />
+        </div>
+
+        <div>
+          <label htmlFor="tomato">Tomato</label>
+          <input
+            type="radio"
+            name="color"
+            id="tomato"
+            checked={state.color === "tomato"}
+            onChange={() =>
+              dispatch({ type: "changeColor", payload: "tomato" })
+            }
+          />
+        </div>
+      </fieldset>
+
+      <fieldset>
+        <legend>Taille de la police :</legend>
+        <select
+          name="size"
+          id="size"
+          onChange={(e) =>
+            dispatch({ type: "changeSize", payload: e.target.value })
+          }
+        >
+          <option value="15px">15px</option>
+          <option value="16px">16px</option>
+          <option value="17px">17px</option>
+          <option value="18px">18px</option>
+          <option value="19px">19px</option>
+          <option value="20px">20px</option>
+        </select>
+      </fieldset>
+
       <ul>
-        {state.texts.map((text, index) => (
-          <li key={index}>{text}</li>
+        {state.texts.map((item, index) => (
+          <li key={index} style={{ color: item.color, fontSize: item.size }}>
+            {item.text}
+            <button
+              style={{ fontSize: "16px" }}
+              onClick={() => dispatch({ type: "deleteEntry", payload: index })}
+            >
+              Supprimer
+            </button>
+          </li>
         ))}
       </ul>
-      <button onClick={() => dispatch({ type: "addInput" })}>Ajouter</button>
+
+      <button
+        onClick={() => dispatch({ type: "addInput" })}
+        disabled={!state.input === ""}
+      >
+        Ajouter
+      </button>
     </>
   );
 };
--- src/reducer/ListReducer.jsx
+++ src/reducer/ListReducer.jsx
@@ -1,6 +1,11 @@
 import { useReducer } from "react";
 
-const initialState = { input: "", texts: [] };
+const initialState = {
+  input: "",
+  texts: [],
+  color: "palevioletred",
+  size: "15px",
+};
 
 const reducer = (state, action) => {
   switch (action.type) {
@@ -11,11 +16,35 @@ const reducer = (state, action) => {
       };
 
     case "addInput":
+      if (state.input === "") return state;
       return {
         ...state,
-        texts: [...state.texts, state.input],
+        texts: [
+          ...state.texts,
+          { color: state.color, size: state.size, text: state.input },
+        ],
         input: "",
       };
+
+    case "changeColor":
+      return {
+        ...state,
+        color: action.payload,
+      };
+
+    case "changeSize":
+      return {
+        ...state,
+        size: action.payload,
+      };
+
+    case "deleteEntry":
+      return {
+        ...state,
+        texts: state.texts.filter((_, index) => index !== action.payload),
+      };
+    default:
+      return state;
   }
 };
 
